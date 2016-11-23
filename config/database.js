9c93d2a84748f8a5dba87a6e8efe5f3367044533
andy jenkins <andygjenkins@gmail.com>
2016-11-23 10:20:33 +0000

    Split app into seperate files

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+      url : 'mongodb://user:pass@waffle.modulusmongo.net:27017/jeH5arob',
+      options : {
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS
+      }
+  };
