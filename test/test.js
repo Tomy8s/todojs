5883383507c183d1f0c1159b44bfdf676ddc9d52
andy jenkins <andygjenkins@gmail.com>
2016-11-23 18:21:17 +0000

    add some passing tests

--- test/test.js
+++ test/test.js
@@ -15,14 +15,33 @@ describe('toDoJS', function() {
         this.browser.visit('/', done);
     });
 
+
     //Tests begin
     it('should return 200 status', function() {
-        assert.ok(this.browser.success)
+        assert.ok(this.browser.success);
     });
 
     it('should display the app\'s name', function() {
-        assert.equal(this.browser.text('h1'), 'I\'m a Todo-aholic 4'); 
+        assert.equal(this.browser.text('h1'), 'MEANTeam\'s Vietnam');
     });
+
+    after(function(done) {
+        this.browser.pressButton('Delete');
+    });
+
+    // it('should display a placeholder', function() {
+    //     this.browser.assert.attribute('input', 'placeholder', 'test');
+    // });
+
+    it('should allow a todo to be posted', function(done) {
+        var browser = this.browser;
+        browser.fill('#todo-text-input', 'New Todo');
+        browser.pressButton('Add').then(function() {
+          assert.ok(browser.success);
+          assert.equal(browser.text('#todo-list div div:last-child'), 'New Todo');
+        }).then(done, done);
+    });
+
 
     //Tests end
     after(function(done) {
